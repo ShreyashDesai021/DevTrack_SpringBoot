298db80db6b0e3c80d83442da222831b85442d68
Shreyash Desai <dshreyash04@gmail.com>
2026-04-18 00:44:56 +0530

    Change import from User entity to User model

--- devtrack-backend/src/main/java/com/devtrack/controller/UserController.java
+++ devtrack-backend/src/main/java/com/devtrack/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.devtrack.controller;
 
-import com.devtrack.entity.User;
+import com.devtrack.model.User;
 import com.devtrack.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
